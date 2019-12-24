87020a7122854bc46c8c3630e39f55f15bcaef8f
Ali Alobaidi <alinawar2000@gmail.com>
2019-12-24 13:09:00 -0500

    Initial Commit

---
+++ restaurantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  restaurantApp
+//
+//  Created by Ali Alobaidi on 12/24/19.
+//  Copyright © 2019 Ali Alobaidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
