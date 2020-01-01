eff2ccd96cd962393ddd8901cd74990e9819f052
Ali Alobaidi <alinawar2000@gmail.com>
2020-01-01 17:35:09 -0500

    okay

--- restaurantApp/Controllers/Auth/LoginViewController.swift
+++ restaurantApp/Controllers/Auth/LoginViewController.swift
@@ -23,6 +23,7 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        passwordTextField.isSecureTextEntry = true
         for button in buttonCollection {
             button.layer.cornerRadius = 10
         }
@@ -43,7 +44,6 @@ class LoginViewController: UIViewController {
                     self.goToClientOrAdmin(type: user!.type)
                 }
             }
-            
         }
     }
     
